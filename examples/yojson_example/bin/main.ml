70eaf5cb07b2622066986a8408d71761f1c9cb0b
John Whitington <john@coherentgraphics.co.uk>
2024-02-27 15:01:11 +0000

    Skeleton and dune-workspace

--- examples/yojson_example/bin/main.ml
+++ examples/yojson_example/bin/main.ml
@@ -7,8 +7,32 @@ let contents_of_file filename =
     with
       e -> close_in ch; raise e
 
+(* Create some JSON in line as a string *)
+let json_string =
+  ""
+
+(* Print as parse tree *)
+let print_parse_tree () =
+  Format.printf "Parsed tree, printed: %a" Yojson.Safe.pp (Yojson.Safe.from_string json_string)
+  
+(* Print as JSON *)
+let prettyprint () = ()
+
+(* Read some JSON from a file, and add up all numbers in it. *)
+let sum filename = ()
+
+(* Read some JSON from a file, and process it and produce new JSON, and write to file. *)
+let process in_filename out_filename = ()
+
+(* 6. Different kinds of JSON *)
+
+(* 7. Dealing with errors *)
+
+(* 8. Different kinds of prettyprinting *)
 let () =
   match Sys.argv with
-  | [|_; "printparsed"; filename|] ->
-      Format.printf "Parsed: %a" Yojson.Safe.pp (Yojson.Safe.from_string (contents_of_file filename))
+  | [|_; "print_parse_tree"|] -> print_parse_tree ()
+  | [|_; "prettyprint"|] -> prettyprint ()
+  | [|_; "sum"; filename|] -> sum filename
+  | [|_; "process"; in_filename; out_filename|] -> process in_filename out_filename
   | _ -> Printf.eprintf "yojson_example: unknown command line\n"
