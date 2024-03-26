6cf33c91192b73fd4e139cf3bd7c5b6a71ab0ac9
John Whitington <john@coherentgraphics.co.uk>
2024-03-26 18:23:43 +0000

    Char example

--- examples/astring/bin/main.ml
+++ examples/astring/bin/main.ml
@@ -1,6 +1,18 @@
-(* Char module *)
+(* It's recommended to use open here. This introduces strf, replaces ^ and the
+   Char and String modules. *)
+open Astring
+
+(* Char module, which represents single bytes (i.e 0x00...0xFF). *)
 let go_char () =
-  ()
+  let c1 = Char.of_byte 65 in
+  let c2 = Char.of_byte 92 in
+  let c3 = Char.of_byte 255 in
+    Printf.printf
+      "c1 = %s, c2 = %s, c3 = %s\n"
+      (Char.Ascii.escape c1) (Char.Ascii.escape c2) (Char.Ascii.escape c3);
+    Printf.printf
+      "which are ASCII? c1 %b, c2 %b, c3 %b\n"
+      (Char.Ascii.is_valid c1) (Char.Ascii.is_valid c2) (Char.Ascii.is_valid c3)
 
 (* String module, basics. *)
 let go_string_1 () =
