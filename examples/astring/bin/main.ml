b14cfa2aaebf2560eef9b562e443a616114f31ab
John Whitington <john@coherentgraphics.co.uk>
2024-03-26 17:53:38 +0000

    Skeleton for astring example

--- examples/astring/bin/main.ml
+++ examples/astring/bin/main.ml
@@ -1,7 +1,23 @@
-let go () =
+(* Char module *)
+let go_char () =
+  ()
+
+(* String module, basics. *)
+let go_string_1 () =
+  ()
+
+(* No-copy substrings *)
+let go_string_2 () =
+  ()
+
+(* Traversing strings *)
+let go_string_3 () =
   ()
 
 let () =
   match Sys.argv with
-  | [|_|] -> go ()
+  | [|_; "char"|] -> go_char ()
+  | [|_; "string1"|] -> go_string_1 ()
+  | [|_; "string2"|] -> go_string_2 ()
+  | [|_; "string3"|] -> go_string_3 ()
   | _ -> Printf.eprintf "astring example: unknown command line\n"
