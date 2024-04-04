6d67b7ffd7cc01ca174ca0b1d552565a683a361e
John Whitington <john@coherentgraphics.co.uk>
2024-04-04 14:41:02 +0100

    Ppx_deriving example done

--- examples/ppx_deriving/bin/main.ml
+++ examples/ppx_deriving/bin/main.ml
@@ -1,38 +1,45 @@
-open Sexplib      (* For Sexplib.Sexp.to_string *)
-open Sexplib.Std  (* To bring int_of_sexp etc required by [@@deriving sexp] *)
+(* ppx_deriving has several plugins, which we must name in our dune file. For
+   example, ppx_deriving.show, ppx_deriving.enum etc. *)
 
-(* Define a tree type, adding an @@deriving annotation *)
+(* ppx_deriving.show can create a printer for a type. *)
 type tree =
   | Lf
-  | Br of tree * int * tree [@@deriving sexp]
+  | Br of tree * int * tree [@@deriving show]
+  (* Generates function show_tree *)
 
-(* This will be rendered as sexp as follows:
+let tr = Br (Lf, 1, Br (Lf, 4, Lf))
 
-      - The int will be converted to a Sexp.Atom using string_of_int
-      - The constructors Lf and Br will be converted to Sexp.Atoms
-      - Br values will be Sexp.List of the three values (the left
-        tree, the integer, the right tree)
-   
-   Two functions, sexp_of_tree and tree_of_sexp will be generated
-   automatically. *)
+let show () =
+  print_endline (show_tree tr)
 
-let rec sum = function
-  | Lf -> 0
-  | Br (l, x, r) -> sum l + x + sum r
+(* ppx_deriving.eq and ppx_deriving.ord can define custom equality. Here, some
+   extra data in a record is ignored with regard to equality and ordering by
+   giving custom functions for equality and comparison to one field. *) 
+type r =
+  {index : int;
+   extra : int [@equal fun _ _ -> true] [@compare fun _ _ -> 0]}
+   [@@deriving eq, ord]
+   (* Generates functions equal_r and compare_r *)
 
-let go () =
-  (* Use the automatically-generated function sexp_of_tree to print our tree *)
-  print_endline (Sexp.to_string (sexp_of_tree (Br (Lf, 1, Br (Lf, 4, Lf)))));
-  (* Use the automatically-generated function tree_of_sexp to sum a sexp *)
-  let tree =
-    tree_of_sexp (List [Atom "Br";
-                        List [Atom "Br"; Atom "Lf"; Atom "7"; Atom "Lf"];
-                        Atom "10";
-                        List [Atom "Br"; Atom "Lf"; Atom "12"; Atom "Lf"]])
-  in
-    Printf.printf "sum: %i\n" (sum tree)
+let eq_ord () =
+  Printf.printf "Equal: %b\n" (equal_r {index = 5; extra = 0} {index = 5; extra = 1});
+  Printf.printf "Compare: %i\n" (compare_r {index = 5; extra = 0} {index = 6; extra = 1});
+  Printf.printf "Compare: %i\n" (compare_r {index = 5; extra = 0} {index = 5; extra = 1})
+
+(* ppx_deriving.enum gives an integer to each argument-less constructor of a datatype *)
+type colour = Blue | Green | Red | Pink [@value 100] | Orange [@@deriving enum, show]
+
+let enum () =
+  Printf.printf "Integers: ";
+  List.iter
+    (fun x -> Printf.printf "%i " (colour_to_enum x))
+    [Blue; Green; Red; Pink; Orange];
+  Printf.printf "\n";
+  Printf.printf "0 is %S\n" (show_colour (match colour_of_enum 2 with Some c -> c | _ -> assert false))
 
 let () =
   match Sys.argv with
-  | [|_|] -> go ()
-  | _ -> Printf.eprintf "ppx_sexp_conv example: unknown command line\n"
+  | [|_; "show"|] -> show ()
+  | [|_; "eq_ord"|] -> eq_ord ()
+  | [|_; "enum"|] -> enum ()
+  | _ -> Printf.eprintf "ppx_deriving example: unknown command line\n"
