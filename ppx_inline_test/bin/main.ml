a01a8bb4322009a1515577d041f952eb582606f1
John Whitington <john@coherentgraphics.co.uk>
2024-04-08 14:49:27 +0100

    ppx_inline_test done

--- ppx_inline_test/bin/main.ml
+++ ppx_inline_test/bin/main.ml
@@ -1,3 +1,16 @@
-let f x = 1
+(* ppx_inline_test doesn't work on executables, so this is a library. Run with
+   "dune runtest" rather than "dune exec" *)
 
-let%test _ = f 2 = 8
+let rec broken = function
+  | ([], []) -> []
+  | (x::xs, []) -> xs
+  | ([], ys) -> ys
+  | (x::xs, y::ys) -> x::y::broken (xs, ys)
+
+let%test _ = broken ([], []) = []
+
+let%test _ = broken ([1; 2; 3], [4; 5; 6]) = [1; 4; 2; 5; 3; 6]
+
+let%test _ = broken ([1; 2; 3], [4; 5]) = [1; 4; 2; 5; 3] (* bug! *)
+
+let%test _ = broken ([1; 2], [3; 4; 5]) = [1; 3; 2; 4; 5]
