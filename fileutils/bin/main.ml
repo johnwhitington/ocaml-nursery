1c024f75bf89814af7dc448ee1cf994ebfe0bbf3
John Whitington <john@coherentgraphics.co.uk>
2024-04-18 15:42:27 +0800

    FilePath examples

--- fileutils/bin/main.ml
+++ fileutils/bin/main.ml
@@ -7,12 +7,18 @@
 
 let go () =
   let filename = "dune" in
-    Printf.printf "Current dir %S\n" FilePath.current_dir;
-    Printf.printf "Parent dir %S\n" FilePath.parent_dir;
+    Printf.printf "Current dir %S\n"
+      FilePath.current_dir;
+    Printf.printf "Parent dir %S\n"
+      FilePath.parent_dir;
     Printf.printf "Make a filename from parts: %S\n"
       (FilePath.make_filename ["one"; "two"; "three"]);
     Printf.printf "Simplify a filename: %S\n"
-      (FilePath.reduce (FilePath.make_filename [FilePath.parent_dir; "one"; FilePath.parent_dir; "two"]))
+      (FilePath.reduce (FilePath.make_filename [FilePath.current_dir; "one"]));
+    Printf.printf "Replace an extension: %S\n"
+      (FilePath.replace_extension "foo.jpg" "jpeg");
+    Printf.printf "Making a PATH string: %S\n"
+      (FilePath.string_of_path ["one"; FilePath.make_filename ["two"; "three"]])
 
 let () =
   match Sys.argv with
