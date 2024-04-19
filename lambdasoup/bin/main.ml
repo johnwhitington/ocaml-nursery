8722a8798e13afa9715a4862439e3d8308b8be47
John Whitington <john@coherentgraphics.co.uk>
2024-04-19 15:25:25 +0800

    lambdasoup skeleton

--- lambdasoup/bin/main.ml
+++ lambdasoup/bin/main.ml
@@ -1,13 +1,7 @@
 let go () =
-  Printf.printf "of_char: %c%c\n" (fst (Hex.of_char 'A')) (snd (Hex.of_char 'A'));
-  Printf.printf "to_char: %C\n" (Hex.to_char '4' '1');
-  Printf.printf "of_string: `Hex %S\n"
-    (match (Hex.of_string "ABCDE") with `Hex x -> x);
-  Printf.printf "to_string: %S\n" (Hex.to_string (`Hex "4142434445"));
-  (* similar functions for bytes, cstrict, and bigstring also exist. *)
-  Hex.hexdump (Hex.of_string "Mary had a little lamb; it's fleece was white as snow.")
+  ()
 
 let () =
   match Sys.argv with
   | [|_|] -> go ()
-  | _ -> Printf.eprintf "hex example: unknown command line\n"
+  | _ -> Printf.eprintf "lambdasoup example: unknown command line\n"
