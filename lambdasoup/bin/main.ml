0a55f3105746890af553052624f159cc773aa494
John Whitington <john@coherentgraphics.co.uk>
2024-04-22 11:01:39 +0800

    First two examples for lambdasoup

--- lambdasoup/bin/main.ml
+++ lambdasoup/bin/main.ml
@@ -1,7 +1,39 @@
-let go () =
+(* Extract text from HTML file, write to stdout *)
+
+(* Bring $, $?, $$ opeators only into scope. *)
+open Soup.Infix
+
+(* Extract a title from an HTML file and print it. We use the optional forms
+   throughout. *)
+let extract () =
+  let soup = Soup.parse (Soup.read_file "example.html") in
+  let title_node = soup $? "title" in
+  match title_node with
+  | Some tn ->
+      begin match Soup.leaf_text tn with
+      | Some lt -> Printf.printf "Title: %S\n" lt
+      | None -> Printf.printf "No leaf text in title."
+      end
+  | None -> Printf.printf "No leaf text"
+
+(* Now the same with exception-raising forms. Much simpler, but coarser error
+   reporting. *)
+let extract_exn () =
+  try
+    Printf.printf
+      "Title: %S\n"
+      (Soup.read_file "example.html" |> Soup.parse $ "title" |> Soup.R.leaf_text)
+  with
+    e ->
+      Printf.printf "Lambdasoup exception: %s\n" (Printexc.to_string e)
+
+(* Process an HTML file, modifying it and writing back to file. *)
+let readwrite () =
   ()
 
 let () =
   match Sys.argv with
-  | [|_|] -> go ()
+  | [|_; "extract"|] -> extract ()
+  | [|_; "extract_exn"|] -> extract_exn ()
+  | [|_; "readwrite"|] -> readwrite ()
   | _ -> Printf.eprintf "lambdasoup example: unknown command line\n"
