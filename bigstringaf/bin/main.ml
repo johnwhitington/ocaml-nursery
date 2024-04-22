e9bfe74e419bc623d9aa89473449f9221f289d69
John Whitington <john@coherentgraphics.co.uk>
2024-04-22 13:58:27 +0800

    Simple bigstringaf example

--- bigstringaf/bin/main.ml
+++ bigstringaf/bin/main.ml
@@ -1,13 +1,21 @@
 let go () =
-  Printf.printf "of_char: %c%c\n" (fst (Hex.of_char 'A')) (snd (Hex.of_char 'A'));
-  Printf.printf "to_char: %C\n" (Hex.to_char '4' '1');
-  Printf.printf "of_string: `Hex %S\n"
-    (match (Hex.of_string "ABCDE") with `Hex x -> x);
-  Printf.printf "to_string: %S\n" (Hex.to_string (`Hex "4142434445"));
-  (* similar functions for bytes, cstrict, and bigstring also exist. *)
-  Hex.hexdump (Hex.of_string "Mary had a little lamb; it's fleece was white as snow.")
+  (* A 20Mb bigstring *)
+  let b = Bigstringaf.create (1024 * 1024 * 20) in
+  (* Get the first megabyte as a new string *)
+  let b2 = Bigstringaf.copy ~off:0 ~len:(1024 * 1024) b in
+  (* Or, as a view on the original string, with no copying *)
+  let b3 = Bigstringaf.sub ~off:0 ~len:(1024 * 1024) in
+  (* Get a string from a section of a bigstring (avoids copy then convert to string) *)
+  let s = Bigstringaf.substring b ~off:0 ~len:(1024 * 1024) in
+  (* Get a 64 bit big endian value *)
+  let i64 = Bigstringaf.get_int64_be b 0 in
+  (* Blitting. Here from string to bigstring *)
+  Bigstringaf.blit_from_string s ~src_off:0 b ~dst_off:(1024 * 1024) ~len:(1024 * 1024);
+  (* Compare *)
+  let cmp = Bigstringaf.memcmp b 0 b2 0 (1024 * 1024) in
+  ()
 
 let () =
   match Sys.argv with
   | [|_|] -> go ()
-  | _ -> Printf.eprintf "hex example: unknown command line\n"
+  | _ -> Printf.eprintf "bigstringaf example: unknown command line\n"
