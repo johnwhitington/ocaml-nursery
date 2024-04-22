6f2065c1d24ab235f001d43e40f45add0debb7b8
John Whitington <john@coherentgraphics.co.uk>
2024-04-22 15:15:32 +0800

    Zarith basic example

--- zarith/bin/main.ml
+++ zarith/bin/main.ml
@@ -1,6 +1,15 @@
 let go () =
-  let z = Z.zero in
-    ()
+  (* Build something bigger than an OCaml int can hold. *)
+  let z = Z.of_int max_int in
+  let bigz = Z.mul z z in
+    print_endline (Z.to_string bigz);
+  (* Go even bigger *)
+  let biggerz = Z.shift_left z 100 in
+    print_endline (Z.to_string biggerz);
+    Printf.printf "This has %i significant bits, of which %i are set\n" (Z.numbits biggerz) (Z.popcount biggerz);
+    Printf.printf "Does it fit in an Int64.t? %b\n" (Z.fits_int64 biggerz);
+    (* Prime just larger than this (probably) *)
+    Printf.printf "Next prime... %s\n" (Z.to_string (Z.nextprime biggerz))
 
 let () =
   match Sys.argv with
