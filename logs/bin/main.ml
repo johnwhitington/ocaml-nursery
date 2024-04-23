fb0d9cc8897a2578e6a50eb6f8a165287d78103a
John Whitington <john@coherentgraphics.co.uk>
2024-04-23 13:19:30 +0800

    simple logs example

--- logs/bin/main.ml
+++ logs/bin/main.ml
@@ -1,7 +1,20 @@
-let go () =
-  ()
+let go warnings =
+  (* Set the logging level *)
+  Logs.set_level (if warnings then Some Logs.Warning else Some Logs.Error);
+  (* Set up the reporter, before calling Logs.err *)
+  Logs.set_reporter (Logs.format_reporter ());
+  let n = 20 in
+    (* Log a warning using a format string *)
+    if n > 5 then
+      Logs.warn (fun m -> m "The number is almost too high: %i" n);
+    if n > 10 then
+      (* Log an error using a format string. *)
+      Logs.err (fun m -> m "The number was too high: %i" n);
+      (* Set exit code based on error count. *)
+      exit (if Logs.err_count () > 0 then 1 else 0)
 
 let () =
   match Sys.argv with
-  | [|_|] -> go ()
+  | [|_; "all"|] -> go true
+  | [|_; "errors"|] -> go false
   | _ -> Printf.eprintf "logs example: unknown command line\n"
